2dc19bb5cf78ddc598688ce3d2cf55ce21232d6a
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-12-11 00:02:26 +0530

    Add Projects component and images

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import Navbar from './components/Navbar'
 import './App.css'
 import Home from './components/Home'
+import Projects from './components/Projects'
 
 function App() {
   
@@ -10,15 +11,11 @@ function App() {
     <main className='h-auto w-screen flex justify-center bg-white text-black'>
       <section className='h-auto items-center flex flex-col w-fit md:w-10/12 '>
         <Navbar />
-        <section id="Home" className='w-full h-auto mb-6 flex flex-col-reverse lg:flex-row lg:justify-evenly items-center gap-2'>
+        <section id="Home" className='w-full h-auto my-6 flex flex-col-reverse lg:flex-row lg:justify-evenly items-center gap-2'>
           <Home />
         </section>
-        <section id="Projects" className='w-full h-screen '>
-          <div className='flex flex-col justify-center items-center'>
-            <h1 className='text-4xl font-bold'>My Projects</h1>
-            <h2 className='text-2xl font-semibold'>A job title</h2>
-            <p className='text-lg font-medium'>A short description about you</p>
-          </div>
+        <section id="Projects" className='w-full h-auto flex items-center justify-center my-6 '>
+          <Projects />
         </section>
         <section id="Contact" className='w-full h-screen'>
           <div className='flex flex-col justify-center items-center'>
